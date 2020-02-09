fe26b1ecdc75b46720f29a7ecf820e9524821945
julien <julien641@yahoo.com>
2020-02-09 06:03:04 -0500

    second commit

---
+++ grader.js
@@ -0,0 +1,69 @@
+module.exports = {
+    compGPA: function(array) {
+        var sum = 0;
+        var totalclass = 0;
+
+        for (var i = 0; i < array.length; i++) {
+
+
+        if (array[i] >= 93 || array[i].toUpperCase() == "A") {
+                sum += 4.00;
+                totalclass++;
+            } else if (array[i] >= 90 || array[i].toUpperCase() == "A-") {
+                sum += 3.67;
+                totalclass++;
+            } else if (array[i] >= 87 || array[i].toUpperCase() == "B+") {
+                sum += 3.330;
+                totalclass++;
+            } else if (array[i] >= 83 || array[i].toUpperCase() == "B") {
+                sum += 3.00;
+                totalclass++;
+            } else if (array[i] >= 80 || array[i].toUpperCase() == "B-") {
+                sum += 2.670;
+                totalclass++;
+            } else if (array[i] >= 77 || array[i].toUpperCase() == "C+") {
+                sum += 2.330;
+            } else if (array[i] >= 73 || array[i].toUpperCase() == "C") {
+                sum += 2;
+                totalclass++;
+            } else if (array[i] >= 70 || array[i].toUpperCase() == "C-") {
+                sum += 1.67;
+                totalclass++;
+            } else if (array[i] >= 67 || array[i].toUpperCase() == "D+") {
+                sum += 1.330;
+                totalclass++;
+            } else if (array[i] >= 63 || array[i].toUpperCase() == "D") {
+                sum += 1.0;
+                totalclass++;
+            } else if (array[i] >= 60 || array[i].toUpperCase() == "D-") {
+                sum += 0.670;
+                totalclass++;
+            } else if (array[i] <= 59 || array[i].toUpperCase() == "F") {
+                sum += 0.0;
+                totalclass++;
+            }
+        }
+        return sum / totalclass;
+    },
+    //TODO
+    
+    student: function (name, CSC141, CSC142, CSC240, CSC241, GPA) {
+        this.name = name.toString();
+        this.CSC141 = CSC141;
+        this.CSC142 = CSC142;
+        this.CSC240 = CSC240;
+        this.CSC241 = CSC241;
+        this.GPA = GPA;
+    },
+    postresult:function(threshhold, studentarray){
+        console.log("The student that has a higher or equal GPA of: " + threshhold);
+        for (var i = 0; i < studentarray.length; i++) {
+            if (studentarray[i].GPA >= threshhold) {
+                console.log("Student Name: " + studentarray[i].name.toString() + " GPA: " + studentarray[i].GPA.toString());
+            }
+        }
+
+
+    }
+  };
+
--- index.js
+++ index.js
@@ -1,123 +1,135 @@
-var prompt = require('prompt');
-var schema = {
+var prompt = require("prompt-async");
+var grader = require('./grader');
+var namescem = {
     properties: {
-        name:{
-            description: 'Enter Name',   
+        name: {
+            description: 'Enter Name',
             type: 'string',
             required: true
-        },
-    CSC141: {
-        description: 'Enter your grade for CSC141',     // Prompt displayed to the user. If not supplied name will be used.
-        type: 'string',                 // Specify the type of input to expect.
-        pattern: /(([af](?!\+|\- ))|(([bcd]+[+-])|[bcd]))(?![A-z ])/gi,                  // Regular expression that input must be valid against.
-        message: 'Your grade must be A B+ B B- C+ C C- D+ D D- F', // Warning message to display if validation fails.
-        hidden: false,                        // If true, characters entered will either not be output to console or will be outputed using the `replace` string.                      // If `hidden` is set it will replace each hidden character with the specified string.
-        required: true                       // If true, value entered must be non-empty.
-    },
-    CSC142: {
-        description: 'Enter your grade for CSC142',     // Prompt displayed to the user. If not supplied name will be used.
-        type: 'string',                 // Specify the type of input to expect.
-        pattern: /(([af](?!\+|\-))|(([bcd]+[+-])|[bcd]))(?![A-z ])/gi,                  // Regular expression that input must be valid against.
-        message: 'Your grade must be A B+ B B- C+ C C- D+ D D- F', // Warning message to display if validation fails.
-        hidden: false,                        // If true, characters entered will either not be output to console or will be outputed using the `replace` string.                      // If `hidden` is set it will replace each hidden character with the specified string.
-                // Default value to use if no value is entered.
-        required: true                      // If true, value entered must be non-empty.
-    },
-            CSC240: {
-            description: 'Enter your grade for CSC240',     // Prompt displayed to the user. If not supplied name will be used.
-            type: 'string',                 // Specify the type of input to expect.
-            pattern: /(([af](?!\+|\- ))|(([bcd]+[+-])|[bcd]))(?![A-z ])/gi,                  // Regular expression that input must be valid against.
-            message: 'Your grade must be A B+ B B- C+ C C- D+ D D- F', // Warning message to display if validation fails.
-            hidden: false,                        // If true, characters entered will either not be output to console or will be outputed using the `replace` string.                      // If `hidden` is set it will replace each hidden character with the specified string.
-                    // Default value to use if no value is entered.
-            required: true                        // If true, value entered must be non-empty.
-                },
-                CSC241: {
-                description: 'Enter your grade for CSC241',     // Prompt displayed to the user. If not supplied name will be used.
-                type: 'string',                 // Specify the type of input to expect.
-                pattern: /(([af](?!\+|\- ))|(([bcd]+[+-])|[bcd]))(?![A-z ])/gi,                  // Regular expression that input must be valid against.
-                message: 'Your grade must be A B+ B B- C+ C C- D+ D D- F', // Warning message to display if validation fails.
-                hidden: false,                        // If true, characters entered will either not be output to console or will be outputed using the `replace` string.                      // If `hidden` is set it will replace each hidden character with the specified string.
-                        // Default value to use if no value is entered.
-                required: true                       // If true, value entered must be non-empty.
-                    }
-   
-      }
+        }
     }
-  
-prompt.start();
 
+}
+
+var schema = {
+    properties: {
+        CSC141: {
+            description: 'Enter your grade for CSC141', // Prompt displayed to the user. If not supplied name will be used.
+            type: 'string', // Specify the type of input to expect.
+            pattern: /((?<!.)([0-9]{1,3}(?!.))|(?<!.)a-(?!.)|((?<!.)a(?!.))|((?<!.)b\+(?!.))|((?<!.)b-(?!.))|((?<!.)b(?!.))|((?<!.)c\+(?!.))|((?<!.)c-(?!.))|((?<!.)c(?!.))|((?<!.)d\+(?!.))|((?<!.)d-(?!.))|((?<!.)f(?!.))|((?<!.)d(?!.))|((?<!.)IP(?!.))|((?<!.)NG(?!.))|((?<!.)W)(?!.))|((?<!.)Y)(?!.)|((?<!.)AU)(?!.)|((?<!.)M)(?!.)/gi,
+            message: 'Your grade must be A B+ B B- C+ C C- D+ D D- F 0-100 IP NG W Y AU M', // Warning message to display if validation fails.
+            hidden: false, // If true, characters entered will either not be output to console or will be outputed using the `replace` string.                      // If `hidden` is set it will replace each hidden character with the specified string.
+            required: true // If true, value entered must be non-empty.
+        },
+        CSC142: {
+            description: 'Enter your grade for CSC142', // Prompt displayed to the user. If not supplied name will be used.
+            type: 'string', // Specify the type of input to expect.
+            pattern: /((?<!.)([0-9]{1,3}(?!.))|(?<!.)a-(?!.)|((?<!.)a(?!.))|((?<!.)b\+(?!.))|((?<!.)b-(?!.))|((?<!.)b(?!.))|((?<!.)c\+(?!.))|((?<!.)c-(?!.))|((?<!.)c(?!.))|((?<!.)d\+(?!.))|((?<!.)d-(?!.))|((?<!.)f(?!.))|((?<!.)d(?!.))|((?<!.)IP(?!.))|((?<!.)NG(?!.))|((?<!.)W)(?!.))|((?<!.)Y)(?!.)|((?<!.)AU)(?!.)|((?<!.)M)(?!.)/gi,
+            message: 'Your grade must be A B+ B B- C+ C C- D+ D D- F 0-100 IP NG W Y AU M', // Warning message to display if validation fails.
+            hidden: false, // If true, characters entered will either not be output to console or will be outputed using the `replace` string.                      // If `hidden` is set it will replace each hidden character with the specified string.
+            // Default value to use if no value is entered.
+            required: true // If true, value entered must be non-empty.
+        },
+        CSC240: {
+            description: 'Enter your grade for CSC240', // Prompt displayed to the user. If not supplied name will be used.
+            type: 'string', // Specify the type of input to expect.
+            pattern: /((?<!.)([0-9]{1,3}(?!.))|(?<!.)a-(?!.)|((?<!.)a(?!.))|((?<!.)b\+(?!.))|((?<!.)b-(?!.))|((?<!.)b(?!.))|((?<!.)c\+(?!.))|((?<!.)c-(?!.))|((?<!.)c(?!.))|((?<!.)d\+(?!.))|((?<!.)d-(?!.))|((?<!.)f(?!.))|((?<!.)d(?!.))|((?<!.)IP(?!.))|((?<!.)NG(?!.))|((?<!.)W)(?!.))|((?<!.)Y)(?!.)|((?<!.)AU)(?!.)|((?<!.)M)(?!.)/gi, // Regular expression that input must be valid against.
+            message: 'Your grade must be A B+ B B- C+ C C- D+ D D- F 0-100 IP NG W Y AU M', // Warning message to display if validation fails.
+            hidden: false, // If true, characters entered will either not be output to console or will be outputed using the `replace` string.                      // If `hidden` is set it will replace each hidden character with the specified string.
+            // Default value to use if no value is entered.
+            required: true // If true, value entered must be non-empty.
+        },
+        CSC241: {
+            description: 'Enter your grade for CSC241', // Prompt displayed to the user. If not supplied name will be used.
+            type: 'string', // Specify the type of input to expect.
+            pattern: /((?<!.)([0-9]{1,3}(?!.))|(?<!.)a-(?!.)|((?<!.)a(?!.))|((?<!.)b\+(?!.))|((?<!.)b-(?!.))|((?<!.)b(?!.))|((?<!.)c\+(?!.))|((?<!.)c-(?!.))|((?<!.)c(?!.))|((?<!.)d\+(?!.))|((?<!.)d-(?!.))|((?<!.)f(?!.))|((?<!.)d(?!.))|((?<!.)IP(?!.))|((?<!.)NG(?!.))|((?<!.)W)(?!.))|((?<!.)Y)(?!.)|((?<!.)AU)(?!.)|((?<!.)M)(?!.)/gi, // Regular expression that input must be valid against.
+            message: 'Your grade must be A B+ B B- C+ C C- D+ D D- F 0-100 IP NG W Y AU M', // Warning message to display if validation fails.
+            hidden: false, // If true, characters entered will either not be output to console or will be outputed using the `replace` string.                      // If `hidden` is set it will replace each hidden character with the specified string.
+            // Default value to use if no value is entered.
+            required: true // If true, value entered must be non-empty.
+        },
+        postresult: {
+            description: ' Type y to print out GPAs',
+            type: 'string',
+            message: 'The string inputed is not Y or N',
+            hidden: false,
+            required: true,
+            pattern: /(?<!.)(y)(?!.)|(?<!.)(n)(?!.)/gi
+
+        },
+        entermore: {
+            description: 'Enter Y to continue and N to exit',
+            type: 'string',
+            message: 'The string inputed is not Y or N',
+            hidden: false,
+            required: true,
+            pattern: /(?<!.)(y)(?!.)|(?<!.)(n)(?!.)/gi
+        }
+
+
+
+    }
+}
+var schemathresh = {
+    properties: {
+        threshhold: {
+            description: 'Enter a GPA threshhold between 0-4.0',
+            type: 'number',
+            message: 'The string inputed is not between 0-4.0',
+            hidden: false,
+            required: true,
+            pattern: /(?<!.)[0-4]\.[0-9](?!.)|(?<!.)[0-4](?!.)|(?<!.)[0-4]+\.(?!.)|(?<!.)\.+[0-9](?!.)/gi
+        }
+
+    }
+
+
+}
+var val = false;
 program();
+async function program() {
+    prompt.start();
+    var studentarray = [];
+    do {
+        var {
+            name
+        } = await prompt.get(namescem);
+        var x = {
+            CSC141,
+            CSC142,
+            CSC240,
+            CSC241,
+            entermore,
+            postresult
+        } = await prompt.get(schema);
 
-function program(){
-
-prompt.get(schema,function(err,result){
-var x=[result.CSC141,result.CSC142,result.CSC240,result.CSC241];
-if(result.name){}
-
-console.log(compGPA(x));
-
-});
-}
 
 
+        var array = [x.CSC141, x.CSC142, x.CSC240, x.CSC241];
+        var t = true;
+        for (var i = 0; i < array.length; i++) {
 
+            if (!await array[i].match(/((?<!.)([0-9]{1,3}(?!.))|(?<!.)a-(?!.)|((?<!.)a(?!.))|((?<!.)b\+(?!.))|((?<!.)b-(?!.))|((?<!.)b(?!.))|((?<!.)c\+(?!.))|((?<!.)c-(?!.))|((?<!.)c(?!.))|((?<!.)d\+(?!.))|((?<!.)d-(?!.))|((?<!.)f(?!.))|((?<!.)d(?!.))|((?<!.)IP(?!.))|((?<!.)NG(?!.))|((?<!.)W)(?!.))|((?<!.)Y)(?!.)|((?<!.)AU)(?!.)|((?<!.)M)(?!.)/gi)) {
+                t = false;
 
+            }
 
-}
-function compGPA(array){
-var sum=0;
-var gpavalue={};
+        }
+        if (t == true) {
+            var GPA = await grader.compGPA(array);
+            await studentarray.push(new grader.student(name.toString(), CSC141, CSC142, CSC240, CSC241, GPA));
 
-    for(var i=0;i<array.length;i++){
+            if (postresult.toUpperCase() == "Y") {
+                var tr = {
+                    threshhold
+                } = await prompt.get(schemathresh);
+                await grader.postresult(tr.threshhold, studentarray);
+            }
+        } else {
+            console.log("Incorrect grade reenter");
 
-    switch(array[i].toUpperCase()){
-
-        case "A":
-        sum+=4;
-        break;
-        case "A-":
-        sum+=3.67;
-        break;
-        case "B+":
-        sum+=3.330;
-        break;
-        case "B":
-        sum+=3.00;
-        break;
-        case "B-":
-        sum+=2.670
-        break;
-        case "C+":
-        sum+=2.330;
-        break;
-        case "C":
-        sum+=2;
-        break;
-        case "C-":
-        sum+=1.67;
-        break;
-        case "D+":
-        sum+=1.330;
-         break;
-        case "D":
-         sum+=1.0;
-        break;
-        case "D-":
-         sum+=0.670;
-        break;
-          case "F":
-         sum+=0.0;
-         break;
-         case "Z":
-         sum+=0.000;
-         break;
-}
+        }
+    } while (x.entermore.toUpperCase() == "Y" || postresult.toUpperCase() == "Y");
 
 
 }
-return sum/array.length;
-}
-
-
-function display(){}
